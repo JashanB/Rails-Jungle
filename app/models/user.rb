527383d693588702cbed5f6abee929299fda6de4
Jashan <jashan_brar50@hotmail.com>
2020-02-10 23:14:56 +0000

    add user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates_length_of :password, minimum: 3
   validates_confirmation_of :password
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by(email: email)
+    stripEmail = email.strip
+    lower = stripEmail.downcase
+    user = User.find_by(email: lower)
     if user && user.authenticate(password) 
       return user
     else 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,35 +4,49 @@ RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   describe 'Validations' do
     it "is valid with valid attributes" do
-      user = User.new(name: 'a', email: 'a', password: 'abc', password_confirmation: 'abc')
+      user = User.new(firstname: 'a', lastname: 'a', email: 'a', password: 'abc', password_confirmation: 'abc')
       expect(user).to be_valid
     end
   end
   describe 'Validations' do
     it "is not valid without valid email" do
-      user = User.new(name: 'a', password: 'abc', password_confirmation: 'abc')
+      user = User.new(firstname: 'a', lastname: 'a', password: 'abc', password_confirmation: 'abc')
       expect(user).to_not be_valid
     end
   end
   describe 'Validations' do
     it "is not valid without valid password" do
-      user = User.new(name: 'a', email: 'a')
+      user = User.new(firstname: 'a', lastname: 'a', email: 'a')
       expect(user).to_not be_valid
     end
   end
   describe 'Validations' do
     it "is not valid without valid password length" do
-      user = User.new(name: 'a', email: 'a', password: 'a', password_confirmation: 'a')
+      user = User.new(firstname: 'a', lastname: 'a', email: 'a', password: 'a', password_confirmation: 'a')
       expect(user).to_not be_valid
     end
   end
   describe 'Validations' do
     it "is not valid without valid password confirmation" do
-      user = User.new(name: 'a', email: 'a', password: 'abc', password_confirmation: 'ab')
+      user = User.new(firstname: 'a', lastname: 'a', email: 'a', password: 'abc', password_confirmation: 'ab')
       expect(user).to_not be_valid
     end
   end
   describe '.authenticate_with_credentials' do
-    # examples for this class method here
+    it "is valid with valid attributes" do
+      @user = User.new(firstname: 'a', lastname: 'a', email: 'a@a', password: 'abc', password_confirmation: 'abc')
+      @user.save
+      expect(User.authenticate_with_credentials('a@a', 'abc')).to be_truthy
+    end
+    it "is valid with valid attributes" do
+      @user = User.new(firstname: 'a', lastname: 'a', email: 'a@a', password: 'abc', password_confirmation: 'abc')
+      @user.save
+      expect(User.authenticate_with_credentials('a@a ', 'abc')).to be_truthy
+    end
+    it "is valid with valid attributes" do
+      @user = User.new(firstname: 'a', lastname: 'a', email: 'a@a', password: 'abc', password_confirmation: 'abc')
+      @user.save
+      expect(User.authenticate_with_credentials('A@A ', 'abc')).to be_truthy
+    end
   end
 end
