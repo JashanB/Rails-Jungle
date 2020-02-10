38bff5a286b300150e52305579a2c8b943dc243b
Jashan <jashan_brar50@hotmail.com>
2020-02-10 21:22:50 +0000

    add user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates_length_of :password, minimum: 3
+  validates_confirmation_of :password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.authenticate(password) 
+      return user
+    else 
+      return nil 
+    end
+  end
 end
