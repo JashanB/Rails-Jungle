77d7b7c1afc4ea2c19b58dc8a99926bbec97f72f
Jashan <jashan_brar50@hotmail.com>
2020-02-11 19:58:53 +0000

    finished testing

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -17,12 +17,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see the products page" do
     visit root_path
-
+    within all('header')[1] { find("h4").click }
+    # within ('header') {find('img').click}
+    # find("#container").find('#products-index').find('#products').find('header').click
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    save_and_open_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).has_content ('Name').to be_truthy
   end
 end
